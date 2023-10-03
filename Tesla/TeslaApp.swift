8a923d8cd4e3285333ce97d9da8bd27becdd25c4
fbara <fbara1@yahoo.com>
2023-10-03 18:15:02 -0400

    Initial Commit

---
+++ Tesla/TeslaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaApp.swift
+//  Tesla
+//
+//  Created by Frank Bara on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct TeslaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
